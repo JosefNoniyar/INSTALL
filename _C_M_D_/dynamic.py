70f2a013675386c3a7926b85b8f128d5119961e8
JosefNoniyar <162990240+JosefNoniyar@users.noreply.github.com>
2024-12-30 14:29:48 +0530

    Create dynamic.py

---
+++ _C_M_D_/dynamic.py
@@ -0,0 +1,22 @@
+import os
+
+# Current Version Of This Program
+version = "Version - 1.3.5"
+
+
+# Check Directory & Clone
+D_C_R_DIRECTORY = "SETUP"
+
+DONT_CLONE_REPO = "https://github.com/JosefNoniyar/SETUP.git"
+
+
+# Current working directory
+directory = os.getcwd()
+
+SKIP_D = D_C_R_DIRECTORY
+
+
+# Home directory
+# directory = os.path.expanduser("~")
+
+# SKIP_D = "INSTALL"
