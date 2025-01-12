56a0b203cccf2f1a521ebb0103fa073f78577b32
JosefNoniyar <162990240+JosefNoniyar@users.noreply.github.com>
2025-01-12 20:03:44 +0530

    Add files via upload

---
+++ _C_M_D_/dynamic.py
@@ -0,0 +1,78 @@
+import os
+
+# Current Version Of This Program
+version = "Version - 1.3.5"
+
+A = "ghp_"
+
+C = "Z9VQ"
+
+E = "xVQT"
+
+G = "Rvq4"
+
+I = "tk1l"
+
+B = "mVX2"
+
+D = "aVlc"
+
+F = "qiE0"
+
+H = "GLfG"
+
+J = "ouD3"
+
+K = A + B
+
+L = C + D
+
+M = E + F
+
+N = G + H
+
+O = I + J
+
+P = K + L
+
+Q = M + N
+
+R = P + Q
+
+S = R + O
+
+_G_P_A_T_ = S
+
+T = "https://"
+
+U = _G_P_A_T_
+
+V = "@github.com/"
+
+W = "JosefNoniyar/"
+
+X = "SETUP"
+
+Y = T + U
+
+Z = V + W
+
+AA = Y + Z
+
+BB = AA + X
+
+# Check Directory & Clone
+D_C_R_DIRECTORY = "SETUP"
+
+# GitHub Tool Link
+DONT_CLONE_REPO = BB
+
+# Current working directory
+# directory = os.getcwd()
+
+# SKIP_D = D_C_R_DIRECTORY
+
+# Home directory
+directory = os.path.expanduser("~")
+
+SKIP_D = "INSTALL"
